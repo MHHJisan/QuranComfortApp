9596968c0fecbd76bebe045ecb734ec4617a9d5b
jisan <mhhaque4@gmail.com>
2025-07-03 20:22:39 +0600

    first commit

---
+++ data/verses.js
@@ -0,0 +1,27 @@
+const versesData = {
+  happy: [
+    {
+      arabic:
+        "قُلْ بِفَضْلِ اللَّهِ وَبِرَحْمَتِهِ فَبِذَٰلِكَ فَلْيَفْرَحُوا هُوَ خَيْرٌ مِمَّا يَجْمَعُونَ",
+      bangla:
+        "বলুন, আল্লাহর অনুগ্রহে ও তাঁর রহমতে; সুতরাং তাঁরা যেন এ নিয়েই খুশি হয়",
+      english:
+        "Say, 'In the bounty of Allah and in His mercy - in that let them rejoice'",
+      reference: "Surah Yunus (10:58)",
+    },
+  ],
+  forgiveness: [
+    {
+      arabic:
+        "وَإِنِّي لَغَفَّارٌ لِّمَن تَابَ وَآمَنَ وَعَمِلَ صَالِحًا ثُمَّ اهْتَدَىٰ",
+      bangla:
+        "আমি ক্ষমাকারী তাদের জন্য যারা তওবা করে, বিশ্বাস স্থাপন করে ও সৎকর্ম করে",
+      english:
+        "Indeed, I am Forgiving to whoever repents, believes, and does righteousness",
+      reference: "Surah Taha (20:82)",
+    },
+  ],
+  // Add other categories with verses
+};
+
+export default versesData;
